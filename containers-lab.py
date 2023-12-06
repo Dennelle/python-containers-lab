d5e99ef0946111acfc58c111721e1c2d08328214
Dennelle <dennelle.walton@gmail.com>
2023-12-05 22:05:36 -0500

    complete 4

--- containers-lab.py
+++ containers-lab.py
@@ -18,6 +18,7 @@ print(foods)
 for food in foods:
     print(food)
 
+# Exercise 3
 # Using a for loop, print just the last two food strings from foods.
 # Hint: Use the slice operator to select the last two foods
 
@@ -27,3 +28,50 @@ for food in foods:
 for food in foods:
     last_two = slice(2,4)
     print(foods[last_two])
+
+# Exercise 4
+# Create a dictionary named home_town containing the keys of city, state and population.
+# Print a string with this format:
+# "I was born in city, state - population of population"
+
+home_town = {
+    'city':'Linden',
+    'country':'Guyana',
+    'population':805000
+}
+print(f'I was born in {home_town["city"]}, {home_town["country"]} - population of {home_town["population"]}')
+
+# Exercise 5
+# Iterate over the key: value pairs in home_town and print a string for each item, for example:
+# "city = Arcadia"
+# "state = California"
+# "population = 58000"
+
+
+
+# Exercise 6
+# Create an empty list named cohort.
+
+# Using a for loop to iterate over the students list.
+
+# Hint: Use the enumerate function to provide both the index & student
+
+# Within the for loop, add a dictionary to the cohort list that combines the student's name and the food in the foods list at the same index. Each dictionary will have this shape:
+
+# Iterate over the cohort list, printing out each item (it's not necessary to format the dictionaries).
+{
+   'student': 'Tina',
+   'fav_food': 'Cheeseburger'
+ }
+
+# Exercise 7
+# Using the list of students and a list comprehension, assign to a variable named awesome_students a new list containing strings similar to this:
+# ["Tina is awesome!", "Fred is awesome!", "Wilma is awesome!"]
+# Iterate over the awesome_students list, printing out each string.
+
+
+
+
+# Exercise 8
+# Use a for loop to iterate over a list comprehension that filters the foods tuple to only include food strings that contains the letter a.
+# Within the for loop, print each food string.
