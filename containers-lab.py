c7a21c99f256287441a01bbd87a21afffa530f26
Dennelle <dennelle.walton@gmail.com>
2023-12-05 21:37:44 -0500

    completed question 2

--- containers-lab.py
+++ containers-lab.py
@@ -7,3 +7,13 @@ students = ["Luke", "Asher", "Levi", "Axel"]
 
 print(f'This is the second student {students[1]}')
 print(f'This is the last student {students[-1]}')
+
+# Exercise 2
+# Create a tuple named foods containing the same number of foods (strings) as there are names in the students list.
+# Use a for loop to print out the string "[food goes here] is a good food".
+
+foods = ('chicken','bread','lettuce','tomato')
+print(foods)
+
+for food in foods:
+    print(food)
