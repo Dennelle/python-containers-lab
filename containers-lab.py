008cfbf634b3cdffda88386dee78298b92aee1b1
Dennelle <dennelle.walton@gmail.com>
2023-12-05 21:32:38 -0500

    completed question 1

--- containers-lab.py
+++ containers-lab.py
@@ -2,3 +2,8 @@
 # Create a list named students containing some student names (strings).
 # Print out the second student's name.
 # Print out the last student's name.
+
+students = ["Luke", "Asher", "Levi", "Axel"]
+
+print(f'This is the second student {students[1]}')
+print(f'This is the last student {students[-1]}')
